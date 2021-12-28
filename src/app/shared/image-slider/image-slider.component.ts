685fe78ca12ced4225b9104f4a6cfe8e4c3d05b1
carlos calderon <cdcalderon@gmail.com>
2021-12-28 14:17:17 -0600

    configure image slider

--- src/app/shared/image-slider/image-slider.component.ts
+++ src/app/shared/image-slider/image-slider.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-    selector: 'app-image-slider',
-    templateUrl: './image-slider.component.html',
-    styleUrls: ['./image-slider.component.scss'],
+  selector: 'app-image-slider',
+  templateUrl: './image-slider.component.html',
+  styleUrls: ['./image-slider.component.scss'],
 })
 export class ImageSliderComponent implements OnInit {
-    slides = [{ image: 'assets/images/slider-badging.jpg' }, { image: 'assets/images/slider-badag.jpg' }, { image: 'https://dummyimage.com/600x400/accf44/fff.png' }];
-    constructor() {}
+  cellWidth = '100%';
+  @ViewChild('myCarousel', { static: false }) myCarousel:
+    | { next: () => void }
+    | undefined;
+  //   slides = [
+  //     { image: 'assets/images/slider-badging.jpg' },
+  //     { image: 'assets/images/slider-badag.jpg' },
+  //     { image: 'https://dummyimage.com/600x400/accf44/fff.png' },
+  //   ];
 
-    ngOnInit(): void {}
+  slides = [
+    { path: 'assets/images/slider-badging.jpg' },
+    { path: 'assets/images/slider-badag.jpg' },
+    { path: 'https://dummyimage.com/600x400/accf44/fff.png' },
+  ];
+
+  next() {
+    if (this.myCarousel) {
+      this.myCarousel.next();
+    }
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/shared/image-slider/image-slider.module.ts
+++ src/app/shared/image-slider/image-slider.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageSliderComponent } from './image-slider.component';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 //import { MatCarouselModule } from '@ngmodule/material-carousel';
 
 @NgModule({
   //imports: [CommonModule, MatCarouselModule.forRoot()],
-  imports: [CommonModule],
+  imports: [CommonModule, IvyCarouselModule],
   declarations: [ImageSliderComponent],
   //exports: [MatCarouselModule, ImageSliderComponent],
   exports: [ImageSliderComponent],
